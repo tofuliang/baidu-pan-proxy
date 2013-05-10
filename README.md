c719834ae8801157e12e9af2ef3c5763dcc7ff8c
tofuliang <tofuliang@gmail.com>
2013-05-11 00:06:08 +0800

    升级到v0.6
    更新内容:
    1.OOP化.
    2.添加file方法获取指定文件名的文件.
    3.添加pic方法,输出可调节质量的图片.
    4.修改使用说明,并转移到README.md中.

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-##百度网盘外链代理
-详细用法不加任何参数访问index.php就可看到
+
+##用法
+###普通代理
+单文件:        index.php?/show|get/`shareid`/`uk[/文件名.扩展名]`         
+文件夹:        index.php?/show|get/`shareid`/`uk`/`文件夹路径`/`文件索引[/文件名.扩展名]`         
+文件夹遍历:  index.php?/folder/`shareid`/`uk`/`文件夹路径`/         
+                    
+#####show
+show为显示下载链接(给下载工具调用)
+#####get
+get为直接重定向到文件(伪静态文件,可作音乐/视频在线播放用)         
+#####folder
+folder文件夹遍历是把该目录下所有下载链接揪出来的(给下载工具调用)         
+#####中括号[]
+`[]`内的为可选参数.可以不加                  
+                    
+###特殊代理
+URL模式:      index.php?`baiduUrl`         
+在`?`后直接加上共享文件的url地址,直接重定向到文件的真实地址.                 
+
+指定文件名模式(限文件夹内文件):                    
+index.php?/file/`shareid`/`uk`/`文件夹路径`/`文件名`                   
+直接重定向到文件的真实地址.
+                
+输出图片可调质量图片模式(限文件内图片):                
+index.php?/pic/`shareid`/`uk`/`文件夹路径`/c`输出图片宽度`u`输出图片高度`q`输出图片质量`/`文件名`                   
+
+###使用例子:
+####单文件
+原链接为                    
+`http://pan.baidu.com/share/link?shareid=322284&uk=1963222956`                    
+获取真实地址为                    
+`http://server/index.php?/show/322284/1963222956`                    
+外链地址为                    
+`http://server/index.php?/get/322284/1963222956`                    
+或者可以添加任意文件名,如                    
+`http://server/index.php?/get/322284/1963222956/1.mp3`                    
+
+####文件夹
+#####文件夹中单个文件
+原链接为                    
+`http://pan.baidu.com/share/link?shareid=166708&uk=419822042#dir/path=%2F%E5%9B%BA%E4%BB%B6%2Ftomato%E7%BD%91%E9%80%9F%E4%BC%98%E5%8C%96%2F%E5%85%B6%E5%AE%83`                    
+获取真实地址为                    
+`http://server/index.php?/get/166708/419822042/%2F%E5%9B%BA%E4%BB%B6%2Ftomato%E7%BD%91%E9%80%9F%E4%BC%98%E5%8C%96%2F%E5%85%B6%E5%AE%83/1`          (第1个文件填1,第二个填2,如此类推)                    
+外链地址为                    
+`http://server/index.php?/show/166708/419822042/%2F%E5%9B%BA%E4%BB%B6%2Ftomato%E7%BD%91%E9%80%9F%E4%BC%98%E5%8C%96%2F%E5%85%B6%E5%AE%83/1`          (第1个文件填1,第二个填2,如此类推)                    
+或者可以添加任意文件名,如                    
+`http://server/index.php?/get/166708/419822042/%2F%E5%9B%BA%E4%BB%B6%2Ftomato%E7%BD%91%E9%80%9F%E4%BC%98%E5%8C%96%2F%E5%85%B6%E5%AE%83/1/1.rar`    (第1个文件填1,第二个填2,如此类推)                    
+                    
+#####遍历文件夹并输出所有下载连接
+原链接为                    
+`http://pan.baidu.com/share/link?shareid=166708&uk=419822042#dir/path=%2F%E5%9B%BA%E4%BB%B6%2Ftomato%E7%BD%91%E9%80%9F%E4%BC%98%E5%8C%96%2F%E5%85%B6%E5%AE%83`                    
+输出所有真实地址                    
+`http://server/index.php?/folder/166708/419822042/%2F%E5%9B%BA%E4%BB%B6%2Ftomato%E7%BD%91%E9%80%9F%E4%BC%98%E5%8C%96%2F%E5%85%B6%E5%AE%83/`           (第1个文件填1,第二个填2,如此类推)                    
+                    
+####特殊代理
+#####URL模式
+原链接为                    
+`http://pan.baidu.com/share/link?shareid=322284&uk=1963222956`
+重定向到真实地址                    
+`http://server/index.php?http://pan.baidu.com/share/link?shareid=322284&uk=1963222956`                             
+                    
+#####指定文件名模式
+分享文件夹链接为                    
+`http://pan.baidu.com/share/link?shareid=384500&uk=1963222956#dir/path=%2Fblog_pic`                    
+文件名为                    
+`P1050091.JPG`                   
+重定向到真实地址                  
+`http://server/index.php?/file/384500/1963222956/%2Fblog_pic/P1050091.JPG`                  
+                 
+#####输出图片可调质量图片模式
+分享文件夹链接为                    
+`http://pan.baidu.com/share/link?shareid=384500&uk=1963222956#dir/path=%2Fblog_pic`                    
+文件名为                    
+`P1050091.JPG`                   
+输出限定高为450px,宽为500px,质量为90%的缩略图地址为(按比例根据指定的较短的边缩放)                    
+`http://server/index.php?/pic/384500/1963222956/%2Fblog_pic/c450u500q90/P1050091.JPG`                  
+
+##附录
+把下面的server/index.php修改成相应地址后，添加到收藏夹可以更方便的使用（不支持文件夹）
+```js
+javascript: void((function() { var tmpurl, srcurl = location.href; tmpurl = srcurl.replace('pan.baidu.com/share', 'server/index.php').replace('/link?', '/?/get').replace('shareid=', '\/').replace('&uk=', '\/').replace('#dir/path=', '\/'); var name=prompt('请右键复制URL，或者点击确定直接下载',tmpurl); if(name!=null && name!='') window.open(tmpurl,'');})())
+```
--- index.php
+++ index.php
@@ -1,185 +1,152 @@
 <?php
 // STARTOFFILE
 
-/**
-*
-* @author tofuliang <tofuliang@gmail.com>
-*/
-
-$useage = '
-<pre>
-参数错误!!!(如果确认参数没有错,请检查程序是否有新版本)
-
-使用方法:
-单文件:     index.php?/show|get/shareid/uk[/文件名.扩展名]
-文件夹:     index.php?/show|get/shareid/uk/文件夹路径/文件索引[/文件名.扩展名]
-文件夹遍历: index.php?/folder/shareid/uk/文件夹路径/
-
-show为显示下载链接(给下载工具调用),get为直接重定向到文件(伪静态文件,可作音乐/视频在线播放用)
-文件夹遍历是把该目录下所有下载链接揪出来的(给下载工具调用)
-中括号内的为可选参数
-使用例子:
-
-
-单文件:
--------------------------------========================================++++++++++++++++++++++++++++++++++++++++++++++++++========================================-------------------------------
-
-原链接为
-http://pan.baidu.com/share/link?shareid=322284&uk=1963222956
-获取真实地址为
-http://server/index.php?/show/322284/1963222956
-外链地址为
-http://server/index.php?/get/322284/1963222956
-或者可以添加任意文件名,如
-http://server/index.php?/get/322284/1963222956/1.mp3
-
-0.4版新增单文件URL作为参数
-举例
-原链接为
-http://pan.baidu.com/share/link?shareid=322284&uk=1963222956
-获取真实地址为
-http://server/index.php?http://pan.baidu.com/share/link?shareid=322284&uk=1963222956
-
--------------------------------========================================++++++++++++++++++++++++++++++++++++++++++++++++++========================================-------------------------------
-
-文件夹:
--------------------------------========================================++++++++++++++++++++++++++++++++++++++++++++++++++========================================-------------------------------
-
-原链接为
-http://pan.baidu.com/share/link?shareid=166708&uk=419822042#dir/path=%2F%E5%9B%BA%E4%BB%B6%2Ftomato%E7%BD%91%E9%80%9F%E4%BC%98%E5%8C%96%2F%E5%85%B6%E5%AE%83
-获取真实地址为
-http://server/index.php?/show/166708/419822042/%2F%E5%9B%BA%E4%BB%B6%2Ftomato%E7%BD%91%E9%80%9F%E4%BC%98%E5%8C%96%2F%E5%85%B6%E5%AE%83/1          (第1个文件填1,第二个填2,如此类推)
-外链地址为
-http://server/index.php?/show/166708/419822042/%2F%E5%9B%BA%E4%BB%B6%2Ftomato%E7%BD%91%E9%80%9F%E4%BC%98%E5%8C%96%2F%E5%85%B6%E5%AE%83/1          (第1个文件填1,第二个填2,如此类推)
-或者可以添加任意文件名,如
-http://server/index.php?/show/166708/419822042/%2F%E5%9B%BA%E4%BB%B6%2Ftomato%E7%BD%91%E9%80%9F%E4%BC%98%E5%8C%96%2F%E5%85%B6%E5%AE%83/1/1.rar    (第1个文件填1,第二个填2,如此类推)
-
--------------------------------========================================++++++++++++++++++++++++++++++++++++++++++++++++++========================================-------------------------------
-
-文件夹遍历:
--------------------------------========================================++++++++++++++++++++++++++++++++++++++++++++++++++========================================-------------------------------
-
-原链接为
-http://pan.baidu.com/share/link?shareid=166708&uk=419822042#dir/path=%2F%E5%9B%BA%E4%BB%B6%2Ftomato%E7%BD%91%E9%80%9F%E4%BC%98%E5%8C%96%2F%E5%85%B6%E5%AE%83
-获取真实地址为
-http://server/index.php?/folder/166708/419822042/%2F%E5%9B%BA%E4%BB%B6%2Ftomato%E7%BD%91%E9%80%9F%E4%BC%98%E5%8C%96%2F%E5%85%B6%E5%AE%83/           (第1个文件填1,第二个填2,如此类推)
-
--------------------------------========================================++++++++++++++++++++++++++++++++++++++++++++++++++========================================-------------------------------
-
-</pre>项目主页 <a href="https://github.com/tofuliang/baidu-pan-proxy">https://github.com/tofuliang/baidu-pan-proxy</a>
-'."<br /><br /><br />把下面的server/index.php修改成相应地址后，添加到收藏夹可以更方便的使用（不支持文件夹）<pre>javascript: void((function() { var tmpurl, srcurl = location.href; tmpurl = srcurl.replace('pan.baidu.com/share', 'server/index.php').replace('/link?', '/?/get').replace('shareid=', '\/').replace('&uk=', '\/').replace('#dir/path=', '\/'); var name=prompt('请右键复制URL，或者点击确定直接下载',tmpurl); if(name!=null && name!='') window.open(tmpurl,'');})())</pre>
-";
-$localVersion = '0.4';
-$checkFrequency = 86400; // 每隔多少秒到服务器检测更新,默认是一天
-
-if (checkNew ( $localVersion, $checkFrequency ))
-    updateFile ( $localVersion );
-
 // URL的正则
-$reg_url = '/http:\/\/pan\.baidu\.com.*(?:shareid=(\d+)&(?:uk=(\d+?)))/Uis';
+$url = '/http:\/\/pan\.baidu\.com.*(?:shareid=(\d+)&(?:uk=(\d+?)))/Uis';
 
 // 文件夹的正则
-$reg_folder = '/\/(get|show|folder)\/(\d+)\/(\d+)\/(.+)(?:\/(\d+)[\/]((?:.+)\.(?:.+?))|\/(\d+?)[\/]{0,1}|\/)/Uis';
+$folder = '/\/(get|show|folder)\/(\d+)\/(\d+)\/(.+)(?:\/(\d+)[\/]((?:.+)\.(?:.+?))|\/(\d+?)[\/]{0,1}|\/)/Uis';
 
 // 单文件的正则
-$reg_file = '/\/(get|show)\/(\d+)\/(\d+?)(?:[\/]((?:.+)\.(?:.+?))|[\/]{0,1})/Uis';
+$file = '/\/(get|show)\/(\d+)\/(\d+?)(?:[\/]((?:.+)\.(?:.+?))|[\/]{0,1})/Uis';
 
-if (0 == preg_match ( $reg_url, $_SERVER ["QUERY_STRING"], $match ))
-    if (0 == preg_match ( $reg_folder, $_SERVER ["QUERY_STRING"], $match )) {
-        if (0 == preg_match ( $reg_file, $_SERVER ["QUERY_STRING"], $match ))
-            die ( $useage );
-// 如果三个表达式都不匹配,说明参数有误,退出
+//文件夹指定文件名的正则
+$fileName = '/\/(file)\/(\d+)\/(\d+)\/(.+)\/(.+?)/Uis';
+
+//文件夹指定文件名的正则(扩展:为图片是增加尺寸,质量等参数)
+$picName = '/\/pic\/(\d+)(?#shareid)\/(\d+)(?#uk)\/(.+)(?#path)\/(c\d+?)(u\d+?)q(\d+?)(?#图片参数)\/(.+?)(?#文件名)/Uis';
+
+$preg=array('url'=>$url,'folder'=>$folder,'file'=>$file,'picName'=>$picName,'fileName'=>$fileName);
+
+class BaiduPanProxy{
+    private $uk;
+    private $shareid;
+    private $path; //文件夹路径
+    private $file; //指定输出为图片时的文件名
+    private $size; //指定输出为图片时的尺寸
+    private $method; //输出方法
+    private $folderIndex; //文件夹的文件索引号
+    private $folder;//文件夹的json内容
+    private $url='';
+    private $preg=array();
+    // public $trueLinkPreg='|(http:[\\\/]{2,6}www\.baidupcs\.com[\\\/]{1,3}file[\\\/]{1,3}.*)\\\"|U'; //已失效
+    public $trueLinkPreg='|(http:[\\\/]{2,6}d\.pcs\.baidu\.com[\\\/]{1,3}file[\\\/]{1,3}.*)\\\"|U';
+
+    public function __construct($url,$preg) {
+        $this->url = $url;
+        $this->preg = $preg;
     }
+    private function parseUrl(){
+        foreach ($this->preg  as $k=>$value) {
+            if(0 != preg_match ( $value, $this->url, $match )){
+                switch ($k) {
+                    case 'url':
+                        list ( , $this->shareid, $this->uk) = $match;
+                        $this->method='get';//此情况下默认是get,不能自定义
+                        $this->matchBy='url';
+                        break;
 
-// 遍历文件夹列出所有文件
-    if (5 == count ( $match ) && 'folder' == $match [1])
-        list ( , $method, $shareid, $uk, $path ) = $match;
+                    case 'folder':
+                        if(8 == count ( $match )) // 文件夹,不带文件名
+                            list ( , $this->method, $this->shareid, $this->uk, $this->path,,,$this->folderIndex) = $match;
+                        if(5 == count ( $match )) // 文件夹,遍历
+                            list ( , $this->method, $this->shareid, $this->uk, $this->path) = $match;
+                        if(7 == count ( $match )) // 文件夹,带文件名
+                            list ( , $this->method, $this->shareid, $this->uk, $this->path,$this->folderIndex) = $match;
+                        $this->folderIndex=$this->folderIndex-1;
+                        $this->matchBy='folder';
+                        // var_dump($match);exit();
+                        break;
 
-// 单文件
-    if (4 <= count ( $match ) &&  'folder' != $match [1])
-        list ( , $method, $shareid, $uk, $filename ) = $match;
+                    case 'file':
+                        list ( , $this->method,$this->shareid, $this->uk) = $match;
+                        $this->matchBy='file';
+                        break;
 
-// 文件夹,不带文件名
-    if (8 == count ( $match ) && 'folder' != $match [1])
-        list ( , $method, $shareid, $uk, $path, , , $index ) = $match;
+                    case 'picName':
+                        list ( , $this->shareid, $this->uk, $this->path, $c,$u,$q,$this->file) = $match;
+                        $this->size='size='.$c.'_'.$u.'&quality='.$q;
+                        $this->file=urldecode($this->file);
+                        $this->matchBy='picName';
+                        break;
 
-// 文件夹,带文件名
-    if (7 == count ( $match ) && 'folder' != $match [1])
-        list ( , $method, $shareid, $uk, $path, $index, $filename ) = $match;
-
-    if (3 == count ( $match )) {
-        list ( ,$shareid, $uk ) = $match;
-        $method = 'get';
+                    case 'fileName':
+                        list ( , ,$this->shareid, $this->uk, $this->path,$this->file) = $match;
+                        $this->file=urldecode($this->file);
+                        $this->matchBy='fileName';
+                        break;
+                }
+                return $this;
+            }
+        }
+        if(!$match) $this->error();//匹配不到任何正确的参数则退出
     }
-
-// 最重要两个参数木有就退出
-    if (! $shareid || ! $uk)
-        die ( $useage );
-
-// 递归挖掘目录函数
-    function dig($path = '', $shareid = '', $uk = '') {
+    private function parseFolder($url){
+        $html=file_get_contents($url);
+        return json_decode($html,true);
+    }
+    private function searchFolder(){
+        $url = "http://pan.baidu.com/share/list?dir=" . $this->path . "&shareid=" . $this->shareid . "&uk=" . $this->uk . "&";
+        $this->json=$this->parseFolder($url);
+        if(! is_array($this->json)) $this->error();
+    }
+    private function getFolderIndex($list){
+        foreach ($list as $k=>$v) {
+            if(strtolower($v['server_filename'])==strtolower($this->file) )return $k;
+        }
+    }
+    private function digFolder($path = '', $shareid = '', $uk = ''){
         $url = "http://pan.baidu.com/share/list?dir=" . $path . "&shareid=" . $shareid . "&uk=" . $uk . "&";
-        $url = file_get_contents ( $url );
-        $url = json_decode ( $url, true );
-        if (! is_array ( $url ))
-            die ( $useage );
-        foreach ( $url ['list'] as $arr ) {
-// var_dump($arr);
+        $this->json=$this->parseFolder($url);
+        if(! is_array($this->json)) $this->error();
+        foreach ( $this->json ['list'] as $arr ) {
             if (1 == $arr ['isdir'])
-                dig ( urlencode ( $arr ['path'] ), $shareid, $uk );
+                $this->digFolder ( urlencode ( $arr ['path'] ), $shareid, $uk );
             else
-                echo ("Download link:<a href=\"" . $arr ['dlink'] . "\">" . $arr ['server_filename'] . "</a><br />");
+                $this->str .= "<a href=\"" . $arr ['dlink'] . "\">" . $arr ['server_filename'] . "</a><br />";
+        }
+        return $this->str;
+    }
+    private function getRealLink(){
+        if(!$this->method){//如果没有方法,只有输入真实文件名或输出可调质量图片两种方法
+            $this->searchFolder();
+            $this->folderIndex=$this->getFolderIndex($this->json['list']);
+            if($this->size){
+                //调节图片输出质量
+                $this->realLink=preg_replace("/size=c\d+_u\d+&quality=\d+/", $this->size, $this->json['list'][$this->folderIndex]['thumbs']['url3']);
+            }else{
+                $this->realLink=$this->json['list'][$this->folderIndex]['dlink'];
+            }
+        }elseif ('folder' == $this->method) {
+            $this->realLink = $this->digFolder($this->path,$this->shareid ,$this->uk);
+        }else{
+            if(!$this->path){
+                $url = "http://pan.baidu.com/share/link?shareid=" . $this->shareid . "&uk=" . $this->uk;
+                $html = file_get_contents ( $url );
+                if (0 == preg_match ( $this->trueLinkPreg, $html, $url ))
+                    $this->error();
+                $_string = array ("replace" => Array ("&amp;", '\\' ), "string" => Array ("&", "" ) );
+                // 替换转义字符
+                $this->realLink = str_ireplace ( $_string ["replace"], $_string ["string"], $url [1] );
+            }elseif ($this->path) {
+                $this->searchFolder();
+                $this->realLink=$this->json['list'][$this->folderIndex]['dlink'];
+            }
         }
     }
-    if ('folder' == $method) {
-// 输出文件夹链接
-        dig ( $path, $shareid, $uk );
-    } elseif ('show' == $method) {
-// 输出下载链接
-        if ($path) {
-            $url = "http://pan.baidu.com/share/list?dir=" . $path . "&shareid=" . $shareid . "&uk=" . $uk . "&";
-            $url = file_get_contents ( $url );
-            $url = json_decode ( $url, true );
-            if (! is_array ( $url ))
-                die ( $useage );
-// var_dump($url);
-            $url = $url ['list'] [$index - 1] ['dlink'];
-            if ($url)
-                exit ( "Download link:<a href=\"" . $url . "\">" . $url . "</a>" );
-            exit ( $useage );
-        } else {
-            $url = "http://pan.baidu.com/share/link?shareid=" . $shareid . "&uk=" . $uk;
-            $url = file_get_contents ( $url );
-            if (0 == preg_match ( '|(http:[\\\/]{2,6}www\.baidupcs\.com[\\\/]{1,3}file[\\\/]{1,3}.*)\\\"|U', $url, $url ))
-                exit ( $useage );
-            $_string = array ("replace" => Array ("&amp;", '\\' ), "string" => Array ("&", "" ) );
-// 替换转义字符
-            $url = str_ireplace ( $_string ["replace"], $_string ["string"], $url [1] );
-            exit ( "Download link:<a href=\"" . $url . "\">" . $url . "</a>" );
-        }
-    } elseif ('get' == $method) {
-// 输出重定向
-        if ($path) {
-            $url = "http://pan.baidu.com/share/list?dir=" . $path . "&shareid=" . $shareid . "&uk=" . $uk . "&";
-            $url = file_get_contents ( $url );
-            $url = json_decode ( $url, true );
-// print_r($url);
-            if (! is_array ( $url ))
-                die ( $useage );
-            $url = $url ['list'] [$index - 1] ['dlink'];
-            header ( "Location:" . $url );
-        } else {
-            $url = "http://pan.baidu.com/share/link?shareid=" . $shareid . "&uk=" . $uk;
-            $url = file_get_contents ( $url );
-            if (0 == preg_match ( '|(http:[\\\/]{2,6}d\.pcs\.baidu\.com[\\\/]{1,3}file[\\\/]{1,3}.*)\\\"|U', $url, $url ))
-                exit ( $useage );
-            $_string = array ("replace" => Array ("&amp;", '\\' ), "string" => Array ("&", "" ) );
-// 替换转义字符
-            $url = str_ireplace ( $_string ["replace"], $_string ["string"], $url [1] );
-            header ( "Location:" . $url );
-        }
+    public function haveFun(){
+        if ($this->checkNew ( $this->localVersion, $this->checkFrequency ))
+            $this->updateFile ( $this->localVersion );
+        $this->parseUrl()->getRealLink();
+        if('show'==$this->method || 'folder'==$this->method)
+            echo $this->realLink;
+        else
+            header ( "Location:" . $this->realLink );
     }
-    function checkNew($localVersion, $checkFrequency) {
+    private function error(){
+        die('oops!! 获取失败诶~建议到<a href="https://github.com/tofuliang/baidu-pan-proxy" target="_blank">https://github.com/tofuliang/baidu-pan-proxy</a>了解了解吧~');
+    }
+    private function checkNew($localVersion, $checkFrequency) {
         $lastCheck = ( int ) @file_get_contents ( __DIR__ . "/.lastcheck" );
         if (time () - $lastCheck > $checkFrequency) {
             @file_put_contents ( __DIR__ . "/.lastcheck", time () );
@@ -190,10 +157,17 @@ if (0 == preg_match ( $reg_url, $_SERVER ["QUERY_STRING"], $match ))
         }
         return false;
     }
-    function updateFile($localVersion) {
+    public function updateFile($localVersion) {
         $new = ( string ) @file_get_contents ( "https://raw.github.com/tofuliang/baidu-pan-proxy/master/index.php" );
         if (strpos ( $new, 'STARTOFFILE' ) && strpos ( $new, 'ENDOFFILE' )) {
             copy ( __FILE__, __FILE__ . $localVersion ) && @file_put_contents ( __FILE__, $new ) && exit ( "代理程序已更新到新版" );
         }
     }
+
+
+}
+$link = new BaiduPanProxy($_SERVER ["QUERY_STRING"],$preg);
+$link->localVersion = '0.6';
+$link->checkFrequency = 86400; // 每隔多少秒到服务器检测更新,默认是一天
+$link->haveFun();
 //ENDOFFILE
